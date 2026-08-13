Evolution-X/frameworks_base

SystemUI: Properly configure wallpaper picker for vanilla and gapps builds
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

packages/SystemUI/src/com/android/systemui/keyguard/util/WallpaperPickerIntentUtils.kt
@@ -18,22 +18,24 @@ package com.android.systemui.keyguard.util
 
 import android.content.Context
 import android.content.Intent
-import com.android.systemui.res.R
+
+import com.android.internal.util.evolution.Utils
 
 /** Provides function(s) to get intent for launching the Wallpaper Picker app. */
 object WallpaperPickerIntentUtils {
 
     fun getIntent(context: Context, launchSource: String): Intent {
+        val isGoogleWpInstalled = Utils.isPackageInstalled(context, GOOGLE_WP_PKG)
+        val wpPkg = if (isGoogleWpInstalled) GOOGLE_WP_PKG else DEFAULT_WP_PKG
         return Intent(Intent.ACTION_SET_WALLPAPER).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK
-            context
-                .getString(R.string.config_wallpaperPickerPackage)
-                .takeIf { it.isNotEmpty() }
-                ?.let { packageName -> setPackage(packageName) }
+            setPackage(wpPkg)
             putExtra(WALLPAPER_LAUNCH_SOURCE, launchSource)
         }
     }
 
     private const val WALLPAPER_LAUNCH_SOURCE = "com.android.wallpaper.LAUNCH_SOURCE"
     const val LAUNCH_SOURCE_KEYGUARD = "app_launched_keyguard"
+    private const val DEFAULT_WP_PKG = "com.android.wallpaper"
+    private const val GOOGLE_WP_PKG = "com.google.android.apps.wallpaper"
 }

Inventor365/frameworks_base-lunaris

Signed-off-by: Ghosuto <clash.raja10@gmail.com>

packages/SystemUI/src/com/android/systemui/keyguard/util/WallpaperPickerIntentUtils.kt
@@ -18,22 +18,24 @@ package com.android.systemui.keyguard.util
 
 import android.content.Context
 import android.content.Intent
-import com.android.systemui.res.R
+
+import com.android.internal.util.lunaris.Utils
 
 /** Provides function(s) to get intent for launching the Wallpaper Picker app. */
 object WallpaperPickerIntentUtils {
 
     fun getIntent(context: Context, launchSource: String): Intent {
+        val isGoogleWpInstalled = Utils.isPackageInstalled(context, GOOGLE_WP_PKG)
+        val wpPkg = if (isGoogleWpInstalled) GOOGLE_WP_PKG else DEFAULT_WP_PKG
         return Intent(Intent.ACTION_SET_WALLPAPER).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK
-            context
-                .getString(R.string.config_wallpaperPickerPackage)
-                .takeIf { it.isNotEmpty() }
-                ?.let { packageName -> setPackage(packageName) }
+            setPackage(wpPkg)
             putExtra(WALLPAPER_LAUNCH_SOURCE, launchSource)
         }
     }
 
     private const val WALLPAPER_LAUNCH_SOURCE = "com.android.wallpaper.LAUNCH_SOURCE"
     const val LAUNCH_SOURCE_KEYGUARD = "app_launched_keyguard"
+    private const val DEFAULT_WP_PKG = "com.android.wallpaper"
+    private const val GOOGLE_WP_PKG = "com.google.android.apps.wallpaper"
 }