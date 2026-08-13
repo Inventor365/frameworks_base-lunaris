yaap/frameworks_base

base: Add a toggle for long press on navbar to search [1/3]

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -183,7 +183,8 @@ public class SystemSettings {
                 Settings.System.QS_BT_AUTO_ON,
                 Settings.System.BRIGHTNESS_SLIDER_HAPTICS,
                 Settings.System.VOLUME_PANEL_HAPTICS,
-                Settings.System.THREE_FINGER_GESTURE
+                Settings.System.THREE_FINGER_GESTURE,
+                Settings.System.NAVBAR_LONG_PRESS_GESTURE
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);

Inventor365/frameworks_base-lunaris

Change-Id: I306f00fb349bb79b4a7faaecc34f1716f27d031a
Signed-off-by: Ghosuto <clash.raja10@gmail.com>

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -129,7 +129,8 @@ public class SystemSettings {
                 Settings.System.PREFERRED_REGION,
                 Settings.System.CV_ENABLED,
                 Settings.System.CV_DYNAMIC_ENABLED,
-                Settings.System.CV_PREFERRED_INTENSITY
+                Settings.System.CV_PREFERRED_INTENSITY,
+                Settings.System.NAVBAR_LONG_PRESS_GESTURE
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);